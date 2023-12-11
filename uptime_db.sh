3108c03d46588356f01a2dbabbdac4ef2f096133
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-12-11 09:41:45 +0100

    Fix spelling error in table day_of_week wednesdey
    Corrected a spelling error in table day_of_week to improve code readability and maintain consistency. The error was in line number 9, where the word "wednesdey" was corrected to "wednesday". This commit addresses the issue and ensures a more accurate representation of the intended code.

--- uptime_db.sh
+++ uptime_db.sh
@@ -6,7 +6,7 @@ wtmp_to_sqlite () {
         "CREATE TEMP TABLE IF NOT EXISTS tmp(type TEXT NOT NULL, datetime TEXT NOT NULL UNIQUE, date TEXT NOT NULL, time TEXT);"\
         "CREATE TABLE IF NOT EXISTS wtmp(id INTEGER PRIMARY KEY, type TEXT NOT NULL, datetime TEXT NOT NULL UNIQUE, date TEXT NOT NULL, time TEXT, uptime INTEGER);"\
         "CREATE TABLE IF NOT EXISTS day_of_week (id INTEGER NOT NULL UNIQUE, dow TEXT NOT NULL UNIQUE);"\
-        "INSERT INTO day_of_week (id, week) VALUES(1, 'monday'), (2, 'tuesday'), (3, 'wednesdey'), (4, 'thursday'), (5, 'friday'), (6, 'saturday'), (7, 'sunday') ON CONFLICT DO NOTHING;"\
+        "INSERT INTO day_of_week (id, week) VALUES(1, 'monday'), (2, 'tuesday'), (3, 'wednesday'), (4, 'thursday'), (5, 'friday'), (6, 'saturday'), (7, 'sunday') ON CONFLICT DO NOTHING;"\
         ".import ./wtmp.csv tmp"\
         "CREATE TRIGGER IF NOT EXISTS insert_wtmp AFTER INSERT ON wtmp BEGIN UPDATE wtmp SET uptime = strftime('%s', (SELECT datetime FROM wtmp WHERE id = NEW.id)) - strftime('%s', (SELECT datetime FROM wtmp WHERE id = (NEW.id - 1) AND type = 'reboot')) WHERE id = NEW.id AND type = 'shutdown'; end;"\
         "INSERT OR IGNORE INTO wtmp(type, datetime, date, time) SELECT type, datetime, date, time FROM tmp;"\
