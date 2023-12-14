b5bd4f351b17e140be41953d692f90137d25783d
Karl Wulfert <pic16f877.ccs@emailn.de>
2023-12-14 20:13:11 +0100

    Version v0.1.1

--- uptime_db.sh
+++ uptime_db.sh
@@ -3,7 +3,7 @@
 # The bash script creates a SQLite3 database using data, date and time (reboot, shutdown) from /var/log/wtmp
 #
 # Version information
-SCRIPT_VERSION="0.1.0"
+SCRIPT_VERSION="0.1.1"
 #
 
 bin_to_txt() {
